b2e381fc09647d5fe51e2bbb3d7e5cab28a0bf02
mamediarra1996 <diarramameboussogueye@gmail.com>
2023-01-11 21:39:46 +0000

    commit

---
+++ src/main/java/com/groupeisi/demo/SpringBootDockerApplication.java
@@ -0,0 +1,21 @@
+package com.groupeisi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+
+    @GetMapping("/message")
+    public String getMessage(){
+        return "welcom to diarra..!!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerApplication.class, args);
+    }
+
+}
